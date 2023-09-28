f5c9b14bb41fb66e08bf2aad75dd5cd1e561a56f
Luka Vukovic <vukovicluka33@gmail.com>
2023-09-28 19:17:12 +0200

    Sign up and login process with validation

---
+++ firebase/firebase-config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6ESU7nWO7Le0h-v8LT0mD8N6ccowdGbc",
+  authDomain: "js-project-23be3.firebaseapp.com",
+  databaseURL: "https://js-project-23be3-default-rtdb.firebaseio.com",
+  projectId: "js-project-23be3",
+  storageBucket: "js-project-23be3.appspot.com",
+  messagingSenderId: "555756532637",
+  appId: "1:555756532637:web:d104162ca8c1f1cfd6b574"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut };
---
+++ js-users/signup.js
@@ -0,0 +1,40 @@
+import { auth, createUserWithEmailAndPassword } from "../firebase/firebase-config.js";
+
+const email = document.querySelector("#email");
+const password = document.querySelector("#pass");
+const signUpForm = document.querySelector("#signup-form");
+
+const userSignUp = async () => {
+  const emailVal = email.value;
+  const passVal = password.value;
+  
+  createUserWithEmailAndPassword(auth, emailVal, passVal)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      window.location.href = "../home.html";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+      if (errorCode === "auth/email-already-in-use"){
+        const element = document.createElement("p");
+        const textnode = document.createTextNode("Email is already taken!");
+        email.style.border = "1px solid red";
+        element.style.color = "red";
+        element.style.margin = "8px 0";
+        element.appendChild(textnode);
+      
+        document.querySelector("#email-container").appendChild(element);
+      }
+      else {
+        console.error("Error:", errorCode, errorMessage);
+      }
+      signUpForm.reset();
+    });
+};
+
+signUpForm.addEventListener('submit', function (e) {
+  e.preventDefault(); 
+  userSignUp();
+});
